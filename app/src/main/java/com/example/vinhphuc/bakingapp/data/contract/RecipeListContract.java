23530649cef9da5bcee300c131f6747859e93cfa
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-25 00:46:06 +0300

    Add Recipe List Contract class

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/contract/RecipeListContract.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/contract/RecipeListContract.java
@@ -1,4 +1,22 @@
 package com.example.vinhphuc.bakingapp.data.contract;
 
+import com.example.vinhphuc.bakingapp.data.MvpView;
+import com.example.vinhphuc.bakingapp.data.model.Recipe;
+import com.example.vinhphuc.bakingapp.data.presenter.MvpPresenter;
+import com.example.vinhphuc.bakingapp.idlingresource.RecipesIdlingResource;
+
+import java.util.List;
+
 public interface RecipeListContract {
+
+    interface View extends MvpView {
+        void showRecipes(List<Recipe> recipes);
+        void showToRecipeDetails(Recipe recipe);
+    }
+
+    interface Presenter extends MvpPresenter<View> {
+        void loadRecipes(RecipesIdlingResource idlingResource);
+        void navigateToRecipeSteps(Recipe recipe);
+    }
+
 }
