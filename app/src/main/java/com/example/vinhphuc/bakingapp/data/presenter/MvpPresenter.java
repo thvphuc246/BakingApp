ed05012228db48a104a40e51f9bfe605a5aa2740
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-26 23:15:16 +0300

    Implement interface MvpPresenter

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/presenter/MvpPresenter.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/presenter/MvpPresenter.java
@@ -1,4 +1,12 @@
 package com.example.vinhphuc.bakingapp.data.presenter;
 
-interface MvpPresenter {
+import com.example.vinhphuc.bakingapp.data.MvpView;
+
+/**
+ * Every presenter in the app must either implement this interface or extend BasePresenter
+ * indicating the MvpView type that wants to be attached with.
+ */
+public interface MvpPresenter<V extends MvpView> {
+    void attachView(V mvpView);
+    void detachView();
 }
