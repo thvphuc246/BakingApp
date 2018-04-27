a76881d4d0658db62d990c5bfa71044d1776e7d4
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-28 00:04:37 +0300

    Add class Base Presenter

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/presenter/BasePresenter.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/presenter/BasePresenter.java
@@ -1,4 +1,49 @@
 package com.example.vinhphuc.bakingapp.data.presenter;
 
-class BasePresenter {
+import com.example.vinhphuc.bakingapp.data.MvpView;
+
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class BasePresenter<T extends MvpView> implements MvpPresenter<T> {
+    private CompositeSubscription compositeSubscription = new CompositeSubscription();
+    private T view;
+
+    @Override
+    public void attachView(T mvpView) {
+        this.view = checkNotNull(mvpView, "View cannot be null");
+    }
+
+    @Override
+    public void detachView() {
+        view = null;
+        compositeSubscription.clear();
+    }
+
+    public T getView() {
+        checkViewAttached();
+        return view;
+    }
+
+    protected void checkViewAttached() {
+        if (!isViewAttached())
+            throw new MvpViewNotAttachedException();
+    }
+
+    private boolean isViewAttached() {
+        return view != null;
+    }
+
+    protected void addSubscription(Subscription subscription) {
+        this.compositeSubscription.add(subscription);
+    }
+
+    public static class MvpViewNotAttachedException extends RuntimeException {
+        MvpViewNotAttachedException() {
+            super("Please call presenter.attachView(MvpView) before requesting data from the " +
+                    "Presenter");
+        }
+    }
 }
