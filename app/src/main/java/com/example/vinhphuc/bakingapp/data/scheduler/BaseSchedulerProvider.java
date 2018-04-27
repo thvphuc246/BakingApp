18e3a724c89562b178dcad7850af3951b1c3d7e0
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-28 00:32:22 +0300

    Add class Base Scheduler Provider

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/scheduler/BaseSchedulerProvider.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/scheduler/BaseSchedulerProvider.java
@@ -1,4 +1,19 @@
 package com.example.vinhphuc.bakingapp.data.scheduler;
 
-class BaseSchedulerProvider {
+import android.support.annotation.NonNull;
+
+import rx.Scheduler;
+
+/**
+ * Allow providing different types of {@link Scheduler}s.
+ */
+public interface BaseSchedulerProvider {
+    @NonNull
+    Scheduler computation();
+
+    @NonNull
+    Scheduler io();
+
+    @NonNull
+    Scheduler ui();
 }
