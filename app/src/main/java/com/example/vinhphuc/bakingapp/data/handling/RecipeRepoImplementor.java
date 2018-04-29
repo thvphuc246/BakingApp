b4d2e5596992ae1fbd5d17a96d9e583a9d0ee1de
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-29 04:02:57 +0300

    Add Recipe Repo Implementor class

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/handling/RecipeRepoImplementor.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/handling/RecipeRepoImplementor.java
@@ -1,4 +1,30 @@
 package com.example.vinhphuc.bakingapp.data.handling;
 
-class RecipeRepoImplementor {
+import com.example.vinhphuc.bakingapp.data.RecipeRepo;
+import com.example.vinhphuc.bakingapp.data.model.ApiService;
+import com.example.vinhphuc.bakingapp.data.model.Recipe;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.functions.Func0;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class RecipeRepoImplementor implements RecipeRepo {
+    private final ApiService apiService;
+
+    public RecipeRepoImplementor(ApiService apiService) {
+        this.apiService = checkNotNull(apiService, "Baking App Api Service cannot be null");
+    }
+
+    @Override
+    public Observable<List<Recipe>> getRecipes() {
+        return Observable.defer(new Func0<Observable<List<Recipe>>>() {
+            @Override
+            public Observable<List<Recipe>> call() {
+                return apiService.fetchRecipes();
+            }
+        });
+    }
 }
