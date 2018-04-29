c38f01c144374d42ac72bf52a4d00d9c1a317fc9
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-29 04:03:33 +0300

    Add Scheduler Provider class

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/scheduler/SchedulerProvider.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/scheduler/SchedulerProvider.java
@@ -1,4 +1,41 @@
 package com.example.vinhphuc.bakingapp.data.scheduler;
 
-class SchedulerProvider {
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class SchedulerProvider implements BaseSchedulerProvider {
+    @Nullable
+    private static SchedulerProvider INSTANCE;
+
+    //Make constructor private to prevent direct instantiation
+    private SchedulerProvider() { }
+
+    public static synchronized SchedulerProvider getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new SchedulerProvider();
+        }
+        return INSTANCE;
+    }
+
+    @Override
+    @NonNull
+    public Scheduler computation() {
+        return Schedulers.computation();
+    }
+
+    @Override
+    @NonNull
+    public Scheduler io() {
+        return Schedulers.io();
+    }
+
+    @Override
+    @NonNull
+    public Scheduler ui() {
+        return AndroidSchedulers.mainThread();
+    }
 }
