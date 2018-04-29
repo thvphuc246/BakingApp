b0037b001ad7bf1b7a7473b19660f62c5d8e3efb
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-29 04:41:09 +0300

    Add Recipe List Adapter class

--- app/src/main/java/com/example/vinhphuc/bakingapp/RecipeListAdapter.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/RecipeListAdapter.java
@@ -1,4 +1,4 @@
-package com.example.vinhphuc.bakingapp.adapter;
+package com.example.vinhphuc.bakingapp;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,7 +8,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.example.vinhphuc.bakingapp.R;
 import com.example.vinhphuc.bakingapp.data.model.Recipe;
 
 import java.util.ArrayList;
