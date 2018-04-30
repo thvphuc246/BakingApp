82ee2537a86661fe686c461c7e0400156b2c1f55
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-30 14:29:10 +0300

    Add Recipe Details Presenter interface

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/presenter/RecipeDetailsPresenter.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/presenter/RecipeDetailsPresenter.java
@@ -1,4 +1,50 @@
 package com.example.vinhphuc.bakingapp.data.presenter;
 
-class RecipeDetailsPresenter {
+import com.example.vinhphuc.bakingapp.data.contract.RecipeDetailsContract;
+import com.example.vinhphuc.bakingapp.data.model.Recipe;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class RecipeDetailsPresenter
+        extends BasePresenter<RecipeDetailsContract.View>
+        implements RecipeDetailsContract.Presenter {
+    private Recipe mCurrentRecipe;
+
+    public RecipeDetailsPresenter(Recipe currentRecipe) {
+        this.mCurrentRecipe = checkNotNull(currentRecipe);
+    }
+
+    @Override
+    public void attachView(RecipeDetailsContract.View mvpView) {
+        super.attachView(mvpView);
+        loadRecipeName();
+        loadIngredients();
+        loadSteps();
+    }
+
+    @Override
+    public void loadRecipeName() {
+        getView().showRecipeNameInAppBar(mCurrentRecipe.getName());
+    }
+
+    @Override
+    public void loadIngredients() {
+        getView().showIngredients(mCurrentRecipe.getIngredients());
+    }
+
+    @Override
+    public void loadSteps() {
+        getView().showSteps(mCurrentRecipe.getSteps());
+    }
+
+    @Override
+    public void loadStepData(int stepId) {
+        getView().showStepsDetailContainer(stepId);
+    }
+
+    @Override
+    public void navigateToStepDetails(int stepId) {
+        checkViewAttached();
+        getView().showStepDetails(stepId);
+    }
 }
