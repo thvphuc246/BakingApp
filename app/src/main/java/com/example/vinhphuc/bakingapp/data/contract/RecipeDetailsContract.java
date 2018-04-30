1a54a303568cffad423578fdd9a00caef865be1c
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-30 14:28:22 +0300

    Add Recipe Details Contract interface

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/contract/RecipeDetailsContract.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/contract/RecipeDetailsContract.java
@@ -1,4 +1,27 @@
 package com.example.vinhphuc.bakingapp.data.contract;
 
+import com.example.vinhphuc.bakingapp.data.MvpView;
+import com.example.vinhphuc.bakingapp.data.model.Ingredient;
+import com.example.vinhphuc.bakingapp.data.model.Step;
+import com.example.vinhphuc.bakingapp.data.presenter.MvpPresenter;
+
+import java.util.List;
+
 public interface RecipeDetailsContract {
+    interface View extends MvpView {
+        void showSteps(List<Step> steps);
+        void showIngredients(List<Ingredient> ingredients);
+        void showRecipeNameInAppBar(String recipeName);
+        void showStepDetails(int stepId);
+        void showStepsDetailContainer(int stepId);
+        void setPresenter(Presenter presenter);
+    }
+
+    interface Presenter extends MvpPresenter<RecipeDetailsContract.View> {
+        void loadRecipeName();
+        void loadIngredients();
+        void loadSteps();
+        void loadStepData(int stepId);
+        void navigateToStepDetails(int stepId);
+    }
 }
