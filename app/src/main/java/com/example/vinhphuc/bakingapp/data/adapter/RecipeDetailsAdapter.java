517ce88323e68ad594663a6aecc9ecee86a596db
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-05-03 19:04:08 +0300

    Add class Recipe Details Adapter

--- app/src/main/java/com/example/vinhphuc/bakingapp/data/adapter/RecipeDetailsAdapter.java
+++ app/src/main/java/com/example/vinhphuc/bakingapp/data/adapter/RecipeDetailsAdapter.java
@@ -1,4 +1,118 @@
 package com.example.vinhphuc.bakingapp.data.adapter;
 
-class RecipeDetailsAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.vinhphuc.bakingapp.R;
+import com.example.vinhphuc.bakingapp.data.model.Step;
+
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindColor;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class RecipeDetailsAdapter extends RecyclerView.Adapter<RecipeDetailsAdapter.ViewHolder> {
+    private final OnStepClickListener mStepClickListener;
+    private List<Step> steps;
+
+    private int currentPosition;
+
+    RecipeDetailsAdapter(List<Step> steps, OnStepClickListener stepClickListener) {
+        this.mStepClickListener = stepClickListener;
+        setSteps(steps);
+    }
+
+    private void setSteps(List<Step> steps) {
+        this.steps = checkNotNull(steps);
+    }
+
+    void replaceData(List<Step> steps) {
+        setSteps(steps);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecipeDetailsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.recipe_step_list_item, parent, false);
+        return new RecipeDetailsAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeDetailsAdapter.ViewHolder holder, int position) {
+        if (steps == null) {
+            return;
+        }
+        holder.bind(steps.get(position), position, mStepClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (steps == null)
+            return 0;
+        return steps.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.step_layout)
+        RelativeLayout stepItemLayout;
+        @BindView(R.id.step_description)
+        TextView stepDescription;
+        @BindView(R.id.step_video_icon)
+        ImageView videoIcon;
+        @BindColor(R.color.colorGrayBackground)
+        int normalItemBackground;
+        @BindColor(R.color.colorPrimaryLight)
+        int currentItemBackground;
+
+        private int currentId;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        void bind(final Step step, int position, final OnStepClickListener itemClickListener) {
+            currentId = step.getId();
+
+            String description = step.getShortDescription();
+            stepDescription.setText(String.format(Locale.ENGLISH, "%d. %s", currentId, description));
+            String video = step.getVideoURL();
+
+            if (video.isEmpty()) {
+                videoIcon.setVisibility(View.INVISIBLE);
+            } else {
+                videoIcon.setVisibility(View.VISIBLE);
+            }
+
+            if (currentPosition == position) {
+                stepItemLayout.setBackgroundColor(currentItemBackground);
+            } else {
+                stepItemLayout.setBackgroundColor(normalItemBackground);
+            }
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    currentPosition = currentId;
+                    itemClickListener.onStepClicked(step);
+                    notifyDataSetChanged();
+                }
+            });
+        }
+    }
+
+    interface OnStepClickListener {
+        void onStepClicked(Step step);
+    }
 }
