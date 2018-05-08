49caaffdfccbe667c10637b0376600f920bd8b03
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-05-08 16:17:43 +0300

    Add adapter class Recipe Step Page

---
+++ app/src/main/java/com/example/vinhphuc/bakingapp/presentation/recipe_step/RecipeStepPageAdapter.java
@@ -0,0 +1,44 @@
+package com.example.vinhphuc.bakingapp.presentation.recipe_step;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.vinhphuc.bakingapp.R;
+import com.example.vinhphuc.bakingapp.data.model.Step;
+
+import java.util.List;
+import java.util.Locale;
+
+class RecipeStepPageAdapter extends FragmentPagerAdapter {
+    private List<Step> steps;
+    private final String tabTitle;
+
+    RecipeStepPageAdapter(FragmentManager fm, List<Step> steps, Context context) {
+        super(fm);
+        setSteps(steps);
+        tabTitle = context.getResources().getString(R.string.recipe_step_tab_label);
+    }
+
+    public void setSteps(@NonNull List<Step> steps) {
+        this.steps = steps;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return SingleStepFragment.newInstance(steps.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return steps.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return String.format(Locale.US, tabTitle, position);
+    }
+}
