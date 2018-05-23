68c0205bfb0edf8979e48bec690771b085461383
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-05-23 19:54:36 +0300

    Add Recipe List Activity UI test class

---
+++ app/src/androidTest/java/com/example/vinhphuc/bakingapp/presentation/recipe_list/RecipeListActivityUITest.java
@@ -0,0 +1,58 @@
+package com.example.vinhphuc.bakingapp.presentation.recipe_list;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.vinhphuc.bakingapp.R;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeListActivityUITest {
+
+    @Rule
+    public ActivityTestRule<RecipeListActivity> mActivityTestRule =
+            new ActivityTestRule<>(RecipeListActivity.class);
+
+    private IdlingResource idlingResource;
+
+    @Before
+    public void registerIdlingResource() {
+        idlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(idlingResource);
+    }
+
+    @Test
+    public void onRecipeListActivityOpen_displayRecyclerView() {
+        //Check whether the RecyclerView is displayed
+        onView(withId(R.id.recipe_list)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickRecipeListItem_openRecipeDetailsActivity() {
+        onView(withId(R.id.recipe_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.recipe_details_ingredients))
+                .check(matches(isDisplayed()));
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        if (idlingResource != null) {
+            Espresso.unregisterIdlingResources(idlingResource);
+        }
+    }
+}
