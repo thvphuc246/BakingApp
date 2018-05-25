1e0cac0ff07e90a551173117e858f71c44778387
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-05-25 03:41:50 +0300

    Add Recipe Details Activity UI test class

--- app/src/androidTest/java/com/example/vinhphuc/bakingapp/presentation/recipe_details/RecipeDetailsActivityUITest.java
+++ app/src/androidTest/java/com/example/vinhphuc/bakingapp/presentation/recipe_details/RecipeDetailsActivityUITest.java
@@ -1,4 +1,40 @@
 package com.example.vinhphuc.bakingapp.presentation.recipe_details;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.vinhphuc.bakingapp.R;
+import com.example.vinhphuc.bakingapp.presentation.recipe_list.RecipeListActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class RecipeDetailsActivityUITest {
+    @Rule
+    public ActivityTestRule<RecipeListActivity> activityActivityTestRule =
+            new ActivityTestRule<>(RecipeListActivity.class, false, true);
+
+    @Test
+    public void clickOnRecyclerViewItem_opensRecipeStepActivity() {
+
+        onView(withId(R.id.recipe_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.recipe_details_steps))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.step_viewpager))
+                .check(matches(isDisplayed()));
+    }
 }
